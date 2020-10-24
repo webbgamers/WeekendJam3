4c4858f53cad2742075fd8d52c19a41783017ccf
webbgamers <webbgamers@gmail.com>
2020-10-24 17:48:21 -0400

    added map and path

---
+++ Assets/Scripts/PathController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    // Display the path in editor for ease of use
+    void OnDrawGizmos() {
+        Vector3 startPosition = transform.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach(Transform waypoint in transform) {
+            Gizmos.DrawSphere(waypoint.position, .5f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+    }
+}
